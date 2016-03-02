3c938353a1235293ef41da80350b21fe1870c332
Administrator <hhh>
2016-03-02 16:13:39 +0800

    关联xutils，测试

--- yljapp/src/main/java/com/ylj/main/LoginActivity.java
+++ yljapp/src/main/java/com/ylj/main/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.ylj;
+package com.ylj.main;
 
 import java.util.Locale;
 
@@ -20,7 +20,14 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.ylj.R;
+import com.ylj.common.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+@ContentView(R.layout.activity_login)
+public class LoginActivity extends BaseActivity {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -35,23 +42,16 @@ public class LoginActivity extends AppCompatActivity {
     /**
      * The {@link ViewPager} that will host the section contents.
      */
+    @ViewInject(R.id.container)
     ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -62,29 +62,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
---
+++ yljapp/src/main/java/com/ylj/main/MyApplication.java
@@ -0,0 +1,20 @@
+package com.ylj.main;
+
+import android.app.Application;
+
+import com.ylj.BuildConfig;
+
+import org.xutils.x;
+
+/**
+ * Created by wyouflf on 15/10/28.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        x.Ext.init(this);
+        x.Ext.setDebug(BuildConfig.DEBUG);
+    }
+}
